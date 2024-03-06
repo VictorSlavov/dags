d70488ce9f9f6a4fa68bb6ed65d7db28a557ea6d
Victor Slavov <victor@sessionstack.com>
2024-03-06 16:30:46 +0200

    pyop

---
+++ test_pythonop.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import PythonOperator
+
+default_args = {
+  'owner': 'victor',
+  'retries': 1
+}
+
+def greet():
+  print("hello... nevermind.")
+
+with DAG (
+  dag_id = 'my_dag',
+  default_args = default_args,
+  description = 'This is test DAG',
+  start_date = datetime(2024, 3, 1, 1),
+  schedule_interval = '@daily',
+  catchup = False
+) as dag:
+    task1 = PythonOperator(
+      task_id = 'greet',
+      python_callable = greet
+    )
+    
+    task1
+    
